b47c8c8ff0b427fdd84c17ec94fee76a7d39b436
SimpleImpact <109104323+SimpleImpact@users.noreply.github.com>
2024-10-22 13:56:16 -0500

    Update Control Panel.py

--- Control Panel.py
+++ Control Panel.py
@@ -11,8 +11,16 @@ screen = pygame.display.set_mode((screenx, screeny))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+holder = 0
 
 button = (20, 20, 50, 50)
+
+def button(x, y, w, h):
+    mouseX, mouseY = pygame.mouse.get_pos()
+    if mouseX >= x and mouseX <= x + w and mouseY >= y and mouseY <= y+h:
+        
+        print('New Button')
+        return("New Button")
 
 while running:
     screen.fill("White")
@@ -21,6 +29,12 @@ while running:
     font = pygame.font.SysFont("Algerian", 60, False)
     text = pygame.font.Font.render(font, "+", True, (0, 0, 0))
     screen.blit(text, (20, 20))
+    l, m, r = pygame.mouse.get_pressed(3)
+    print(pygame.mouse.get_pressed(3))
+    if l:
+        button(20, 20, 21, 33)
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT or keys[pygame.K_e]:
             running = False
